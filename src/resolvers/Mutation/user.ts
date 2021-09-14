4d6b6423a78eb32732b79b930756106af8366496
Silvestre Moreno <silmoreno70@gmail.com>
2021-09-13 20:52:17 -0500

    future: expose resolvers

--- src/resolvers/Mutation/user.ts
+++ src/resolvers/Mutation/user.ts
@@ -1,11 +1,20 @@
 import * as bcrypt from "bcryptjs";
+import { UserCreateInput } from "../../generated/prisma-client";
 import { Context } from "../../utils";
 
 export default {
   async setPwd(parent, args, ctx: Context) {
     const { pwd, token } = args.data;
     const password = await bcrypt.hash(pwd, 10);
-    return await ctx.prisma.updateUser({ data: { password }, where: { token_verify: token}});
+    return await ctx.prisma.updateUser({ data: { password }, where: { token }});
   },
-  updateUser: (parent, args, ctx: Context) => ctx.prisma.updateUser(args),
+  updateUser: (parent, args: { data: any, where: any}, ctx: Context) => ctx.prisma.updateUser(args),
+  createUser: (parent, { data }: { data: UserCreateInput}, ctx: Context) => {
+    const token = 'x'
+    data = {
+      token,
+      ...data
+    }
+  return ctx.prisma.createUser(data)
+  }
 };
